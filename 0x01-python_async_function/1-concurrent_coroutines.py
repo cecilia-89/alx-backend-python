78e185062e3190f68a0c924cf1d37fe6e94f7ee6
cecilia-89 <catabong89@gmail.com>
2023-01-09 20:34:47 +0100

    implemented insertion sort

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -6,7 +6,15 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
-    """returns list of delays"""
-    delays = await asyncio.gather(*(wait_random(max_delay) for _ in range(n)))
+async def wait_n(n: int, max_delays: int) -> List[float]:
+    """returns list of delayss"""
+    delays = await asyncio.gather(*(wait_random(max_delays) for _ in range(n)))
+    for i in range(1, len(delays)):
+        key = delays[i]
+        j = i - 1
+        while j >= 0 and key < delays[j]:
+            delays[j + 1] = delays[j]
+            j -= 1
+    delays[j + 1] = key
+
     return delays
--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -8,6 +8,15 @@ task_wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """returns list of delays"""
-    delay = await asyncio.gather(
+    delays = await asyncio.gather(
       *(task_wait_random(max_delay) for _ in range(n)))
-    return delay
+
+    for i in range(1, len(delays)):
+        key = delays[i]
+        j = i - 1
+        while j >= 0 and key < delays[j]:
+            delays[j + 1] = delays[j]
+            j -= 1
+    delays[j + 1] = key
+
+    return delays
