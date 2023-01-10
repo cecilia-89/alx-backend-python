6d56713005d65ae6a2b89f273a2457107c7c308f
cecilia-89 <catabong89@gmail.com>
2023-01-10 15:09:34 +0100

    Asynchronous comprehension

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -10,11 +10,3 @@ async def async_generator() -> List[float]:
     for _ in range(10):
         yield random.uniform(0, 10)
         await asyncio.sleep(1)
-
-async def print_yielded_values():
-    result = []
-    async for i in async_generator():
-        result.append(i)
-    print(result)
-
-asyncio.run(print_yielded_values())
