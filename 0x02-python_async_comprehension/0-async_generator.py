e3985f82a7a22e66be8da53055e96cc4905f127d
cecilia-89 <catabong89@gmail.com>
2023-01-10 15:27:40 +0100

    Asynchronous comprehension

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,7 +5,7 @@ import asyncio
 import random
 
 
-async def async_generator() -> Generator[float, None, None]:
+async def async_generator() -> Generator[float, None]:
     """yields a random number for each iteration"""
     for _ in range(10):
         await asyncio.sleep(1)
