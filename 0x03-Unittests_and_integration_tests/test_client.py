0f9ffbc47b7014d678a9590efa482fbab2a6f22d
cecilia-89 <catabong89@gmail.com>
2023-01-30 11:13:35 +0100

    unittests and intergration

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,21 +2,22 @@
 """Module: test client.py file"""
 import unittest
 from unittest.mock import patch
-from parameterized import parameterized_class
+from parameterized import parameterized
 from client import GithubOrgClient
 
 
-@parameterized_class(('org'), [
-        "google",
-        "abc"
-    ])
+
 class TestGithubOrgClient(unittest.TestCase):
     """"test client module"""
 
+    @parameterized.expand([
+        "google",
+        "abc"
+    ]
+    )
     @patch('utils.get_json')
-    def test_org(self, mock_get_json):
+    def test_org(self, org, mock_get_json):
         """test the org function"""
-        url = GithubOrgClient.ORG_URL.format(org=self.org)
+        url = GithubOrgClient.ORG_URL.format(org=org)
         mock_get_json(url)
         mock_get_json.assert_called_once_with(url)
-
