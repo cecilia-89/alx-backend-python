0ac9563d774341bdaca36917af112c513e2ee2c3
cecilia-89 <catabong89@gmail.com>
2023-01-30 06:10:50 +0100

    unittests and intergration

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,7 +3,7 @@
 
 import unittest
 from unittest.mock import patch
-from utils import access_nested_map
+from utils import access_nested_map, memoize
 from parameterized import parameterized
 
 
@@ -44,3 +44,26 @@ class TestGetJson(unittest.TestCase):
             mock_get_json.assert_called_once_with(test_url)
 
             self.assertEqual(result, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """test the utils.memoize function"""
+    def test_memoize(self):
+        """test memoize function"""
+
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method') as mock_a_method:
+            test_class = TestClass()
+            test_class.a_property()
+            test_class.a_property()
+            mock_a_method.assert_called_once()
+
+
